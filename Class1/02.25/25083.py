9c176a7338cdf37f02518d5b0f88ff896dd366fa
wlstjd6524 <wlstjd6524@naver.com>
2026-02-25 15:02:56 +0900

    feat: BOJ 25083 새싹

---
+++ Class1/02.25/25083.py
@@ -0,0 +1,15 @@
+'''
+- solved.ac Class1 - 백준 25083번 문제 (새싹)
+
+Q. 아래 예제와 같이 새싹을 출력하시오.
+
+Input : 
+
+Print : 새싹을 출력한다.
+'''
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
